1e288feb9cbea46b93fdf76fb608c0d73aa1f8d5
koushik <koushik.dey@live.com>
2022-08-26 00:10:32 +0300

    emjoi

--- src/components/composition/Text.js
+++ src/components/composition/Text.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Text = ({addEmoji}) => {
+const Text = ({ addEmoji }) => {
+    const text = 'I a JavaScript Programming Language.';
   return (
     <div>
+     {addEmoji? addEmoji(text, '❤️') : text }
 
     </div>
   )
 }
+
 
 export default Text
