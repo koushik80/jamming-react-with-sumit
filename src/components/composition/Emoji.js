62698597c3df78e056d53c06cdc5da8438d514e4
koushik <koushik.dey@live.com>
2022-08-26 10:06:20 +0300

    emoji

--- src/components/composition/Emoji.js
+++ src/components/composition/Emoji.js
@@ -1,1 +1,7 @@
-
+import React from 'react';
+export default class Emoji extends React.Component {
+    addEmoji = (text, emoji) => `${emoji} ${text} ${emoji}`;
+    render() {
+        
+     }
+ }
