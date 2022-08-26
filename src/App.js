b47ad47b1bff22c0a6bf2c2f2bab35cb7b10a489
koushik <koushik.dey@live.com>
2022-08-26 22:50:03 +0300

    sh

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import ClickCounter from "./components/ClickCounter";
 import HoverCounter from "./components/HoverCounter";
+import User from "./components/User";
 
 const App = () => {
   return (
       <div className="app">
       <ClickCounter />
       <HoverCounter />
+      <User name="Rajon" />
         </div>
     );
 }
 
 export default App;
+
 
 
