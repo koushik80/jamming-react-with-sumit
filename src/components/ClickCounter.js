61d56b65fe0048c39e150145240bf78959bbcf32
koushik <koushik.dey@live.com>
2022-08-27 01:07:24 +0300

    jk

--- src/components/ClickCounter.js
+++ src/components/ClickCounter.js
@@ -1,15 +1,6 @@
 import React from 'react';
 
 class ClickCounter extends React.Component {
-    state = {
-            count: 0,
-    }
-    incrementCount = () => {
-        this.setState((prevState) => ({
-            count: prevState.count + 1
-        }));
-    };
-
     render() {
         const {count} = this.state;
         return (
