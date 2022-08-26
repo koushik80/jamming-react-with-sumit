d6f657503fddf5af3965e20f06e19dac650f2e65
koushik <koushik.dey@live.com>
2022-08-26 19:26:08 +0300

    React Render Props

---
+++ L-13/src/App.js
@@ -0,0 +1,15 @@
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
+
+const App = () => {
+  return (
+      <div className="app">
+      <ClickCounter />
+      <HoverCounter />
+        </div>
+    );
+}
+
+export default App;
+
+
