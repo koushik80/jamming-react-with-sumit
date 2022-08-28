9155de7b2b5f5e723c6f6341ebb3aa0a2c36feaf
koushik <koushik.dey@live.com>
2022-08-28 20:38:15 +0300

    swt

--- src/App.js
+++ src/App.js
@@ -4,28 +4,37 @@ import Counter from './components/Counter';
 import Section from './components/Section';
 import ThemeContext from './contexts/themeContext';
 
-class App extends React.Component {
-  state = {
-    theme: 'dark'
-  };
-  render() {
-    const { theme } = this.state;
-    return (
-          <div className="app">
-              <Counter>
-                  {(counter, incrementCount) => (
-                      <ClickCounter count={counter} incrementCount={incrementCount} />
-                  )}
-             </Counter>
-        <ThemeContext.Provider value={{ theme: theme }}>
-          <Section />
-        </ThemeContext.Provider>
-          </div>
-       );
+export default class App extends React.Component {
+    state = {
+        // eslint-disable-next-line react/no-unused-state
+        theme: 'light',
+        // eslint-disable-next-line react/no-unused-state
+        switchTheme: () => {
+            this.setState(({ theme }) => {
+                if (theme === 'dark') {
+                    return {
+                        theme: 'light',
+                    };
+                }
+                return {
+                    theme: 'dark',
+                };
+            });
+        },
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <Counter>
+                    {(counter, incrementCount) => (
+                        <ClickCounter count={counter} incrementCount={incrementCount} />
+                    )}
+                </Counter>
+                <ThemeContext.Provider value={this.state}>
+                    <Section />
+                </ThemeContext.Provider>
+            </div>
+        );
     }
 }
-
-export default App;
-
-
-
