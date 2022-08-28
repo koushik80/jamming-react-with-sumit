ff0ef654034842599cbfc859ad878393d611ff4d
koushik <koushik.dey@live.com>
2022-08-28 15:53:58 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React from 'react';
 import ClickCounter from "./components/ClickCounter";
 import Counter from "./components/Counter";
 import Section from "./components/Section";
 
-const App = () => {
-  return (
-    <div className="app">
-      <Counter>
-        {(counter, incrementCount) => (
-           <ClickCounter count={counter} incrementCount={incrementCount} />
-        )}
-      </Counter>
-      <Section />
-    </div>
-    );
+class App extends React.Component {
+  render() {
+      return (
+          <div className="app">
+              <Counter>
+                  {(counter, incrementCount) => (
+                        <ClickCounter count={counter} incrementCount={incrementCount} />
+                  )}
+             </Counter>
+             <Section />
+          </div>
+       );
+    }
 }
 
 export default App;
