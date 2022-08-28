fb8256d760b13cce21f92f44f23976635fb1c183
koushik <koushik.dey@live.com>
2022-08-28 17:09:03 +0300

    value

--- src/lib/Context.js
+++ src/lib/Context.js
@@ -2,4 +2,11 @@ class Context {
     constructor(value = null) {
         this.value = value;
     }
+    // provider
+    
+    Provider = ({ children, value }) => {
+        this.value = value;
+
+    }
+
 }
