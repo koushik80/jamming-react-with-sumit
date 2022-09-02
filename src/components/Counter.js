708b1f1d5f30c5a41eb465ca85dce70146f680d7
koushik <koushik.dey@live.com>
2022-09-02 20:01:14 +0300

    counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 
-const Counter = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    let i = 0;
+    const addFive = () => {
+        while (i < 5) {
+            setCount((prevState) => prevState + 1);
+            i += 1;
+        }
+    };
+
+    return (
+        <div>
+            {count}
+            <p>
+                <button type="button" onClick={() => setCount((prevState) => prevState + 1)}>
+                    Add 1
+                </button>
+            </p>
+            <p>
+                <button type="button" onClick={addFive}>
+                    Add 5
+                </button>
+            </p>
+        </div>
+    );
 }
 
 export default Counter;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 const Todo = () => {
     const [todo, setTodo] = useState('');
-    const [warning, setWarning] = useState(null);
+    const [warning, setWarning] = useState('');
 
 
     const handleInput = (e) => {
