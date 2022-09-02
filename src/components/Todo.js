a3cca72e2794eaca6cedaaf1a91781624948b41b
koushik <koushik.dey@live.com>
2022-09-02 12:58:07 +0300

    jk

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,20 +11,20 @@ const Todo = () => {
             ? 'You need JavaScript skill to complete the task. Do you have it?'
             : null;
 
-        this.setState({
-            todo: inputValue,
-            warning,
-        });
+        setTodo(inputValue);
+        setWarning(warning);
     };
 
   return (
       <div>
           <p>{todo}</p>
-                <p>
-                    <textarea name="todo" value={todo} onChange={this.handleInput} />
-                </p>
-                <hr />
-                <h2>{warning || 'Good choice!'}</h2>
+          <p>
+            <textarea name="todo"
+                value={todo}
+                onChange={this.handleInput} />
+          </p>
+          <hr />
+          <h2>{warning || 'Good choice!'}</h2>
       </div>
   )
 }
