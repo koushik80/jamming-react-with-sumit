b8b64bb2f1eaa7e39ff0f904c43bb5630393263b
koushik <koushik.dey@live.com>
2022-09-03 00:50:16 +0300

    count

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
 const MyComponent = () => {
+    const [count, setCount] = useState(0);
+    const [date, setDate] = useState(new Date());
+
+    const addClick = () => {
+        setCount((prevState) => prevState + 1);
+    };
+
+
   return (
     <div>
       <p>Time: {date.toLocaleTimeString()}</p>
