95f1642b64daccb805ad6b7c72454692f57dad20
koushik <koushik.dey@live.com>
2022-09-03 00:44:52 +0300

    my comp

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <p>Time: {date.toLocaleTimeString()}</p>
+      <p>
+        <button type="button" onClick={addClick}>Click</button>
+      </p>
+    </div>
+  )
+}
+
+export default MyComponent;
