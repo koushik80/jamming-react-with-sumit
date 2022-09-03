6b57a77bed2061761f9ea4f90874d4b7b9a9d995
koushik <koushik.dey@live.com>
2022-09-03 20:20:18 +0300

    kl

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
 import MyComponent from './components/MyComponent';
 
-export default class App extends React.Component {
-    render() {
-        return (
+const App = () => {
+    const [show, setShow] = useState(true);
+
+    return (
+        
             <div className="app">
-              <MyComponent />
+                <div>{show && <MyComponent />}</div>
+                <p>
+                    <button type="button" onClick={() => setShow((prevShow) => !prevShow)}>
+                        {show ? 'Hide post' : 'Show post'}
+                    </button>
+                </p>
             </div>
-        );
-    }
+    );
 }
+
+export default App;
+
+
+
+
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,15 +3,20 @@ import { useEffect, useState } from 'react';
 const MyComponent = () => {
     const [count, setCount] = useState(0);
     const [date, setDate] = useState(new Date());
+    //const [text, setText] = useState('');
 
-    //const tick = () => {
-        //setCount(new Date());
-    //};
+    const tick = () => {
+        setCount(new Date());
+    };
 
     useEffect(() => {
         console.log('updating document title')
-         document.title = `Clicked ${count} times`;
-     });
+        document.title = `Clicked ${count} times`;
+    }, [count]);
+
+    useEffect(() => {
+        setInterval(tick, 1000);
+    }, []);
 
     const addClick = () => {
         setCount((prevState) => prevState + 1);
@@ -20,7 +25,10 @@ const MyComponent = () => {
 
   return (
     <div>
-      <p>Time: {date.toLocaleTimeString()}</p>
+        <p>Time: {date.toLocaleTimeString()}</p>
+          {/*<p>
+              <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+  </p>*/}
       <p>
         <button type="button" onClick={addClick}>Click</button>
       </p>
