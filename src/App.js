9eeae4a18194bde52537b67aca41537233fea099
koushik <koushik.dey@live.com>
2022-09-03 23:19:47 +0300

    count

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
+import Button from './components/Button';
+import ShowCount from './components/ShowCount';
+import Title from './components/Title';
 
 const App = () => {
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  const incrementByOne = () => {
+    setCount1((prevCount) => prevCount + 1);
+
+  };
+
+  const incrementByFive = () => {
+    setCount2((prevCount) => prevCount + 5);
+  }
+
+
+
+
   return (
-    <div>App</div>
+    <div className="app">
+      <Title />
+      <ShowCount />
+      <span></span>
+      <Button></Button>
+      <hr />
+      <ShowCount />
+      <Button></Button>
+    </div>
   )
 }
 
---
+++ src/components/ShowCount.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ShowCount = () => {
+  return (
+    <div>ShowCount</div>
+  )
+}
+
+export default ShowCount;
