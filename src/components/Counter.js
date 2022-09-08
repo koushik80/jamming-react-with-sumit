d818ce013f28f80ad9642c379b106c8ba63b98a8
koushik <koushik.dey@live.com>
2022-09-08 11:47:50 +0300

    counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import { useReducer } from 'react';
+
+const initialState = 0;
+const reducer = (state, action) => {
+
+}
 
 const Counter = () => {
+
+  useReducer(reducer, initialState);
+
+
   return (
     <div>
-      
+      <div>Count -0</div>
+      <button type="button">Increment</button>
+      <button type="button">Decrement</button>
     </div>
-  )
+  );
 }
 
 export default Counter;
