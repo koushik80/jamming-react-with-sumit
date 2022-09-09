aa66697f8c2c2a543f8ddd13a8f740637445fe52
koushik <koushik.dey@live.com>
2022-09-09 15:16:42 +0300

    hooks

--- src/components/LayoutComponent.js
+++ src/components/LayoutComponent.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LayoutComponent = () => {
+  const [onSmallScreen, setOnSmallScreen] = useState();
+
+
+  useEffect(() => {
+
+  }, []);
   return (
     <div>
       <h1>You are browsing on small device</h1>
