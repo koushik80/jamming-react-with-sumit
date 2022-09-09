30071f8f71b0ec2aed39ccaedd9596ffc6fe0a79
koushik <koushik.dey@live.com>
2022-09-09 17:55:24 +0300

    hooks

--- src/components/LayoutComponent.js
+++ src/components/LayoutComponent.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react';
 
 const LayoutComponent = () => {
   const [onSmallScreen, setOnSmallScreen] = useState(false);
-
+  const checkScreenSize = () => {
+    setOnSmallScreen(window.innerWidth < 768);
+  };
 
   useEffect(() => {
-
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
   }, []);
   return (
     <div>
