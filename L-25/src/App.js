21d5607065e8e647f0dc232eb74b922df6391e66
koushik <koushik.dey@live.com>
2022-09-10 01:23:11 +0300

    fg

---
+++ L-25/src/App.js
@@ -0,0 +1,13 @@
+import './assets/css/global.css';
+import Logo from './components/Logo';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Learn with Sumit</h1>
+      <Logo />
+    </div>
+  );
+}
+
+export default App;
