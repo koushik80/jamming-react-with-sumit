bfd1d6fa6091b1e8064c98459cd07917eb58b75b
koushik <koushik.dey@live.com>
2022-09-15 00:39:03 +0300

    dg

--- src/components/styles/Container.styles.js
+++ src/components/styles/Container.styles.js
@@ -4,7 +4,11 @@ import styled from 'styled-components';
 const Container = styled.div`
     display: flex;
     justify-content: center;
-
+    align-items: center;
+    font-family: 'Montserrat', sans-serif;
+    background: #262626;
+    width: 100%;
+    height: 100vh;
 `;
 
 
