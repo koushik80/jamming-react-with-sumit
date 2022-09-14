2ed2ae2caa0002751a9a50d3e3ed7d314efa95eb
koushik <koushik.dey@live.com>
2022-09-15 00:59:49 +0300

    hj

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './assets/css/dark.css';
+import GlobalStyle from './components/styles/Global.styles';
 import Tutorial from './components/Tutorial';
 
-const App = () => {
-
-  return <Tutorial />
+export default function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <Tutorial />
+        </>
+    );
 }
-
-export default App;
