82124d978a6ced0b82fce851bbf63fb5cacdfd28
koushik <koushik.dey@live.com>
2022-10-01 16:16:42 +0300

    kl

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function App() {
     return (
       <div className="container">
-            'Hello World!'
+            <div>Hello World!</div>
       </div>
     );
 }
