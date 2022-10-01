0c000fe44bb998812b97f3cea7b4eca6837e9424
koushik <koushik.dey@live.com>
2022-10-01 18:23:22 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from './components/About';
+import Dashboard from './components/Dashboard';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Posts from './components/Posts';
+import Services from './components/Services';
 
 export default function App() {
     return (
       <Router>
         <Route path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/navbar" component={Navbar} />
+        <Route path="/posts" component={Posts} />
 
       </Router>
     );
