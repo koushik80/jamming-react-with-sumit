a1ca897366ed2beb593d1861c1fd24f48c4ee387
Koushik Dey <koushik.dey@live.com>
2022-11-27 00:51:05 +0200

    clicks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from "react";
+
 
 const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+
+
+const handleLeftClick = () => {
+  setAllClicks(allClicks.concat('L'))
+  setLeft(left + 1)
+}
+
+const handleRightClick = () => {
+  setAllClicks(allClicks.concat('R'))
+  setRight(right + 1)
+}
+
   return (
-    <div>App</div>
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>
+        Left
+      </button>
+      <button onClick={handleRightClick}>
+        Right
+      </button>
+      {right}
+    </div>
   )
 }
 
-export default App
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+const Header = (props) => {
+const course = 'Half Stack application development'
+  return (
+    <div>
+    <h1>{ props.course }</h1>
+    </div>
+  )
+}
+
+export default Header;
+
+
+
+
