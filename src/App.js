ec07fefc327cbe2b47222a3479a2812af281ccf9
Koushik Dey <koushik.dey@live.com>
2022-11-26 22:34:14 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import Courses from "./pages/Courses";
-import Home from "./pages/Home";
-import Layout from "./pages/Layout";
-import NoPage from "./pages/NoPage";
+import React from 'react'
 
-
-export default function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="courses" element={<Courses />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    <div>App</div>
+  )
 }
+
+export default App
