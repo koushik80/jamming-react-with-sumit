690871156f86c16a4d54bf39a602b4c658306586
Koushik Dey <koushik.dey@live.com>
2022-11-29 03:01:43 +0200

    map

--- src/App.js
+++ src/App.js
@@ -12,15 +12,23 @@ export default App;
 
 
 let animals = [
-  {name:'', species:''},
+  { name: 'Fluffykins', species: 'rabbit' },
+  { name: 'Caro', species: 'dog' },
+  { name: 'Hamilton', species: 'dog' },
+  { name: 'Harold', species: 'dog' },
+  { name: 'Ursula', species: 'cat' },
+  { name: 'Jimmy', species: 'fish' }
 ];
 
-let dogs = [];
+/*
+let dogs = animals.filter((animal) => {
+    return animal.species === 'dog';
+});
+*/
 
-for (let i = 0; i < animals.length; i++) {
-  if (animals[i].species === 'dog') {
-    dogs.push(animals[i]);
-  }
-}
+let names = animals.map((animal) => {
+  return animal.name
+})
 
-console.log(dogs);
+
+console.log(names);
