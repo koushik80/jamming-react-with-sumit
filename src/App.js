d256441da06c9dfb0d2f74b54f6ef1b4799cccbf
Koushik Dey <koushik.dey@live.com>
2022-11-29 02:31:32 +0200

    arrays

--- src/App.js
+++ src/App.js
@@ -9,3 +9,18 @@ const App = () => {
 }
 
 export default App;
+
+
+let animals = [
+  {name:'', species:''},
+];
+
+let dogs = [];
+
+for (let i = 0; i < animals.length; i++) {
+  if (animals[i].species === 'dog') {
+    dogs.push(animals[i]);
+  }
+}
+
+console.log(dogs);
