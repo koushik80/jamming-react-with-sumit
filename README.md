d5a3a637c7248e08bfb2b035b97c9ceebaf97c8b
Koushik Dey <koushik.dey@live.com>
2022-11-30 03:26:38 +0200

    exercise

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 <div align="center"><a href="https://www.youtube.com/watch?v=5Xy-t8k_M4A&list=PLHiZ4m8vCp9M6HVQv7a36cp8LKzyHIePr">Jamming with Sumit in React practice pad</a></div>
 <div align="center"><a href="https://fullstackopen.com/en/">Full Stack open 2022</a></div>
 
-<p align="right">To make my concept clearer and keep myself in react-track, I study the course Full-Stack Open and follow Sumit's lessons.</p>
+<p align="center">To make my concept clearer and keep myself in react-track, I study the course Full-Stack Open and follow Sumit's lessons.</p>
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Note from "./components/Note";
+import notes from "./Data";
+
+
+const App = () => {
+  const [notebook, setNotebook] = useState([]);
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  );
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+    content: newNote,
+    date: new Date().toISOString(),
+    important: Math.random() < 0.5,
+    id: notes.length + 1,
+  }
+
+  setNotebook(notes.concat(noteObject))
+  setNewNote('')
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
+
+
+
